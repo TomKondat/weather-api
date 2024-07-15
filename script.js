3f414cf19840a51c81b4d59971957c968298af5a
Tom Kondat <tomkondat5@gmail.com>
2024-07-15 20:51:06 +0300

    some styling

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ const form = document.getElementById("search-bar");
 
 const url = `https://api.weatherapi.com/v1/current.json?key=b8a9505712a14179b93115900241507&q=London&aqi=no`;
 
-let cities = [];
-
 const createCardEl = (data) => {
   const cardEl = document.createElement("div");
   cardEl.innerHTML = `
         <div id="card">
- <img src="${data.current.condition.icon}" alt="${data.current.condition.text}">  <div >
-    <h5 >${data.location.name}</h5>
+ <img src="${data.current.condition.icon}" alt="${data.current.condition.text}"><div>
+    <h5>${data.location.name}, ${data.location.country}</h5>
         <p>${data.current.condition.text}</p>
         <p>${data.current.temp_c}°C</p>
   </div>
@@ -26,11 +24,9 @@ const createCardEl = (data) => {
 const handleSearchCity = (e) => {
   e.preventDefault();
   const city = e.target.children[1].value;
-  const a = getCityWeather(city).then((data) => {
+  getCityWeather(city).then((data) => {
     renderSingle(root, data, createCardEl);
   });
-  cities.push(a);
-  render(root, cities, createCardEl);
 };
 
 //---------------------
@@ -38,11 +34,6 @@ const addTo = (elToAddTo, elToBeAdded) => elToAddTo.append(elToBeAdded);
 const renderSingle = (elToAddTo, dataObj, createCard) => {
   elToAddTo.innerHTML = "";
   addTo(elToAddTo, createCard(dataObj));
-};
-
-const render = (elToAddTo, dataListArr, createCard) => {
-  elToAddTo.innerHTML = "";
-  dataListArr?.map((el) => addTo(elToAddTo, createCard(el)));
 };
 
 //----------------------
